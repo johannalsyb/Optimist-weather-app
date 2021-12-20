71f5c0032097939c39ea03ae5cfea87207c10e28
Johann <johann.alerte@gmail.com>
2021-12-20 23:16:59 +0700

    dek

--- src/app.js
+++ src/app.js
@@ -7,3 +7,20 @@ var monthName = months[today.getMonth()]
 
 let displayDate = document.querySelector("#thedate")
 displayDate.innerHTML = `Today is ${monthName}  ${day}`;
+
+let position = navigator.geolocation.getCurrentPosition;
+
+
+  function handlePosition(position) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    alert("lat:" + lat + " lng:" + lng);
+    codeLatLng(lat, lng);
+    let apikey = "918b8d9899bdd33b4c0152c02d23b5f9";
+    let apiUrl = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${apikey}`
+  
+}
+
+  navigator.geolocation.getCurrentPosition(handlePosition)
+
+
