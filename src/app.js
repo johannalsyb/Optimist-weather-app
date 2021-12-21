a3cb84a56ff042660197982e7b6096e681d45cd7
Johann <johann.alerte@gmail.com>
2021-12-21 09:17:49 +0700

    fj

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+const apiKey = "918b8d9899bdd33b4c0152c02d23b5f9";
+
+
 var today = new Date();
 var monthNumber = (today.getMonth()+1)
 var day = today.getDate();
@@ -11,15 +14,34 @@ displayDate.innerHTML = `Today is ${monthName}  ${day}`;
 let position = navigator.geolocation.getCurrentPosition;
 
 
-  function handlePosition(position) {
+function handlePosition(position) {
     var lat = position.coords.latitude;
-    var lng = position.coords.longitude;
-    alert("lat:" + lat + " lng:" + lng);
-    codeLatLng(lat, lng);
-    let apikey = "918b8d9899bdd33b4c0152c02d23b5f9";
-    let apiUrl = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${apikey}`
-  
+    var lng = position.coords.longitude;  
+    showData(lat,lng)
 }
+
+function showData(lat,lng) {
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=${apiKey}`;
+   axios.get(apiUrl).then(handleData);
+   
+}
+
+function handleData(response){
+  var city = response.data.name;
+
+  var tempCity = Math.round(response.data.main.temp);
+  let h1 = document.querySelector(".card-title");
+ h1.innerHTML = response.data.name;
+
+ let temperature = document.querySelector("#temp");
+ temperature.innerHTML = `${tempCity}°C`;
+  
+ let description = response.data.weather[0].description;
+ let detail = document.querySelector("#description");
+ detail.innerHTML = description;
+}
+
+
 
   navigator.geolocation.getCurrentPosition(handlePosition)
 
