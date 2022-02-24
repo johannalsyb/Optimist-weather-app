b8e9ce9fac79a47103424fb5fb6b3c066e1808ee
Johann <johann.alerte@gmail.com>
2022-02-24 13:30:47 +0000

    dede

--- src/app.js
+++ src/app.js
@@ -71,8 +71,9 @@ const showDataNewQuery = response => {
     var header = document.querySelector(".card-header");
     var search = document.querySelector(".search-bar");
     let tempFirstDay = document.querySelector("#dayplusone");
-    
-      
+    let result = newTempQuery.value;
+    let resultFinal = result.value + 1 ;
+    console.log(resultFinal)
 
     let queryDescription = response.data.weather[0].description;
     let displayDescription = document.querySelector("#description");
